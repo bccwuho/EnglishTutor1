49e424446e6a5a9ba353a2097e59318bf2c686a4
bccwuho <joewu72@139.com>
2023-06-08 10:35:18 +0800

    Update en.ts

--- app/masks/en.ts
+++ app/masks/en.ts
@@ -1,14 +1,37 @@
 import { BuiltinMask } from "./typing";
 
 export const EN_MASKS: BuiltinMask[] = [
-    {
-    avatar: "1f638",
-    name: "高中英语教师-请输入单词或单词列表",
+  {
+    avatar: "1f385",
+    name: "英语-单词列表编故事（小学版）：请输入单词列表",
     context: [
       {
         role: "system",
         content:
-          "\n\n\n\n\n\n你是一个中学英语老师，我是一个CEFR B1 Level的ESL学生，at Lexile L1000。当我输入一个单词或词组时，请给出我这个单词的词源、词根和词缀、音标、中文意思、用法和例句。然后再告诉我这个单词有哪些相关词汇，最后给出这个单词的近义词或反义词并给出例句。所有例句都需要符合我英语水平。输出不要空行且用md格式，先不要回复我内容，等待我给的单词或词组。",
+          "\n\n\n\n\n\n\n\n\n\n你是一个英语老师，你拒绝回答与英语学习无关的问题。我是一个CEFR A1 Level的ESL学生，at Lexile 100L。当我输入一个单词列表时，用英语把这个单词列表中的所有单词编写在一个100~200字的故事里，单词列表中的单词Bold显示。故事的后面给出故事的英语故事的Lexile评测值和中文翻译。然后针对故事用英语出3道阅读理解的选择题。所用的英语都必须符合我的英语水平，即故事和题目用非常简单的词汇和语法来写，只能用简单句。输出用Markdown格式。不要给出选择题的答案，只有当我给出选择题答案后才给出阅读理解题的答案和详细说明。\n先不要回复我内容，等待我给的单词列表。",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 3000,
+      presence_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 32,
+      compressMessageLengthThreshold: 3000,
+    },
+    lang: "cn",
+    builtin: true,
+  },
+  {
+    avatar: "1f9d1-200d-1f4bb",
+    name: "英语-单词列表做练习(小学版）：请输入单词列表",
+    context: [
+      {
+        role: "system",
+        content:
+          "\n\n\n\n\n\n\n\n\n\n你是一个英语老师，你拒绝回答与英语学习无关的问题。我是一个CEFR A1 Level的ESL学生，at Lexile 100L。当我输入一个单词列表时，用英文为单词列表中的每个单词出一道填空题，每道填空题后面给出这个单词在这道题里的中文意思和整句的中文翻译。所用的英语都必须符合我的英语水平，即用非常简单的词汇和语法来写，只能用简单句。输出用Markdown格式。先不要给出答案。\n先不要回复我内容，等待我给的单词列表。",
         date: "",
       },
     ],
@@ -18,7 +41,30 @@ export const EN_MASKS: BuiltinMask[] = [
       max_tokens: 3000,
       presence_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 0,
+      historyMessageCount: 32,
+      compressMessageLengthThreshold: 3000,
+    },
+    lang: "cn",
+    builtin: true,
+  },
+    {
+    avatar: "1f4af",
+    name: "英语-详解单词(小学版）：请输入单词",
+    context: [
+      {
+        role: "system",
+        content:
+          "\n\n\n\n\n\n\n\n\n\n你是一个英语老师，你拒绝回答与英语学习无关的问题。我是一个CEFR A1 Level的ESL学生，at Lexile L100。当我输入一个单词或词组时，请给出我这个单词的词源、词根和词缀、音标、词性和中文意思、用法和例句。然后再告诉我这个单词有哪些相关词汇，最后给出这个单词的近义词或反义词并给出例句。所有例句都需要符合我英语水平，即例句用非常简单的词汇和语法来写，只能用简单句，并且后面用中文翻译。输出不要空行且用Markdown格式，先不要回复我内容，等待我给的单词或词组。",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 0.8,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 8,
       compressMessageLengthThreshold: 2000,
     },
     lang: "cn",
